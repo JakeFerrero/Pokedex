8175f7b227be02a318e9afe271d18a14dceb051e
Jake Ferrero <jake.a.ferrero@gmail.com>
2024-09-18 11:30:57 -0400

    add pokeball to website header

--- src/app/components/utils/WebsiteHeader.tsx
+++ src/app/components/utils/WebsiteHeader.tsx
@@ -1,11 +1,10 @@
-/* eslint-disable @next/next/no-img-element */
 import doge from '../../../../public/doge_head.png';
 import style from './websiteHeader.module.css';
 
 export default function WebsiteHeader() {
   return (
     <div className={style.stickyHeader}>
-      <img src={doge.src} alt='Pokedex' className={style.logo} />
+      <div className={style.pokeball} />
       Pokedex
     </div>);
 }
